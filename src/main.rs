76ec2ab9987cf14ba12517703a5165315d62efd3
Jeff Martin <jmartin@homeaway.com>
2016-08-03 16:24:26 +0100

    Work on blur

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn main() {
 
     let params = DrawParameters {
         point_size: Some(10.0),
-        polygon_mode: PolygonMode::Line,
+        polygon_mode: PolygonMode::Fill,
         multisampling: true, // Why isn't this having any effect
         ..Default::default()
     };
@@ -62,20 +62,61 @@ fn main() {
 
         void main() {
             vec2 pos = position;
-            pos.x += t;
-            pos.y += noise1((pos.y + t) * 10);
+            pos.x += t + noise1((pos.x + t) * 50) / 10;
+            pos.y += noise1((pos.y + t) * 50) / 10;
             gl_Position = vec4(pos, 0.0, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
+        
         out vec4 color;
         
         uniform sampler2D fb;
+        
+		vec2 iResolution = vec2(1024, 768);
+
+        float normpdf(in float x, in float sigma) {
+			return 0.39894 * exp(-0.5 * x * x / (sigma * sigma)) / sigma;
+		}
 
         void main() {
-            color = vec4(0.48, 0.31, 0.22, 1.0);
+        
+	        vec3 c = texture(fb, gl_FragCoord.xy).rgb;
+        
+	        float r = gl_FragCoord.x / 1024;
+
+	    	//declare stuff
+			const int mSize = 11;
+			const int kSize = (mSize-1)/2;
+			float kernel[mSize];
+			vec3 final_colour = vec3(0.0);
+	
+			//create the 1-D kernel
+			float sigma = 7.0;
+			float Z = 0.0;
+			
+			for (int j = 0; j <= kSize; ++j) {
+				kernel[kSize+j] = kernel[kSize-j] = normpdf(float(j), sigma);
+			}
+			
+			//get the normalization factor (as the gaussian has been clamped)
+			for (int j = 0; j < mSize; ++j) {
+				Z += kernel[j];
+			}
+			
+			//read out the texels
+			for (int i=-kSize; i <= kSize; ++i) {
+				for (int j=-kSize; j <= kSize; ++j) {
+					final_colour += kernel[kSize+j]*kernel[kSize+i]*texture(fb, (gl_FragCoord.xy+vec2(float(i),float(j))) / iResolution.xy).rgb;
+				}
+			}
+			
+			
+	//		color = vec4(final_colour/(Z*Z), 1.0);
+        
+            color = vec4(r, 0.31, 0.22, 1.0);
         }
     "#;
 
