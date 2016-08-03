3c3b501f41adc81af43afee6753f2d15b94ede02
Jeff Martin <jmartin@homeaway.com>
2016-08-03 17:03:50 +0100

    Separate render and blur programs

--- src/main.rs
+++ src/main.rs
@@ -68,13 +68,23 @@ fn main() {
         }
     "#;
 
-    let fragment_shader_src = r#"
+    let fragment_shader_src1 = r#"
         #version 140
-        
+
         out vec4 color;
-        
+
+        void main() {
+            color = vec4(0.48, 0.31, 0.22, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src2 = r#"
+        #version 140
+
+        out vec4 color;
+
         uniform sampler2D fb;
-        
+
 		vec2 iResolution = vec2(1024, 768);
 
         float normpdf(in float x, in float sigma) {
@@ -82,9 +92,9 @@ fn main() {
 		}
 
         void main() {
-        
+
 	        vec3 c = texture(fb, gl_FragCoord.xy).rgb;
-        
+
 	        float r = gl_FragCoord.x / 1024;
 
 	    	//declare stuff
@@ -92,37 +102,38 @@ fn main() {
 			const int kSize = (mSize-1)/2;
 			float kernel[mSize];
 			vec3 final_colour = vec3(0.0);
-	
+
 			//create the 1-D kernel
 			float sigma = 7.0;
 			float Z = 0.0;
-			
+
 			for (int j = 0; j <= kSize; ++j) {
 				kernel[kSize+j] = kernel[kSize-j] = normpdf(float(j), sigma);
 			}
-			
+
 			//get the normalization factor (as the gaussian has been clamped)
 			for (int j = 0; j < mSize; ++j) {
 				Z += kernel[j];
 			}
-			
+
 			//read out the texels
 			for (int i=-kSize; i <= kSize; ++i) {
 				for (int j=-kSize; j <= kSize; ++j) {
-					final_colour += kernel[kSize+j]*kernel[kSize+i]*texture(fb, (gl_FragCoord.xy+vec2(float(i),float(j))) / iResolution.xy).rgb;
+					final_colour += kernel[kSize+j] * kernel[kSize+i] *
+						texture(fb, (gl_FragCoord.xy+vec2(float(i),float(j))) / iResolution.xy).rgb;
 				}
 			}
-			
-			
-	//		color = vec4(final_colour/(Z*Z), 1.0);
-        
-            color = vec4(r, 0.31, 0.22, 1.0);
+
+			color = vec4(final_colour/(Z*Z), 1.0);
+
         }
     "#;
 
-
-    let program = Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+    let program = Program::from_source(&display, vertex_shader_src, fragment_shader_src1, None)
         .unwrap();
+
+    let blur_program =
+        Program::from_source(&display, vertex_shader_src, fragment_shader_src2, None).unwrap();
 
     let mut a_thing = thing::AThing::new();
 
@@ -144,16 +155,22 @@ fn main() {
 
         a_thing = a_thing.next();
 
-        let mut target = display.draw();
+        let target = display.draw();
 
         framebuffer.clear_color(0.92, 0.91, 0.81, 1.0);
 
-        target.clear_color(0.92, 0.91, 0.81, 1.0);
 
         framebuffer.draw(&vertex_buffer,
                   &indices,
                   &program,
-                  &uniform! {t: a_thing.position, fb: &texture},
+                  &uniform! {t: a_thing.position },
+                  &params)
+            .unwrap();
+
+        framebuffer.draw(&vertex_buffer,
+                  &indices,
+                  &blur_program,
+                  &uniform! {t: a_thing.position, fb: &texture },
                   &params)
             .unwrap();
 
