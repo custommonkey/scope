bb6a1ef2a79999f8a780ef26b5b1a7d04a789a59
Jeff Martin <jmartin@homeaway.com>
2016-08-05 10:34:44 +0100

    Clean up thing

--- src/thing.rs
+++ src/thing.rs
@@ -14,22 +14,6 @@ pub struct Vertex {
 
 implement_vertex!(Vertex, position);
 
-fn back() -> Vec<Vertex> {
-
-    let x = 1.0;
-    let y = 0.5;
-
-    return vec![
-        Vertex { position: [-x, -y] },
-        Vertex { position: [x, y] },
-        Vertex { position: [x, -y] },
-        Vertex { position: [-x, -y] },
-        Vertex { position: [x, y] },
-        Vertex { position: [-x, y] },
-    ];
-
-}
-
 impl AThing {
     pub fn new() -> AThing {
         return AThing {
@@ -39,21 +23,34 @@ impl AThing {
         };
     }
 
-    fn shape(&self) -> Vec<Vertex> {
-
-        return (-90..91)
-            .map(|n| Vertex { position: [(n as f32) / 100.0, 0.0] })
-            .collect();
-
-
-    }
-
     pub fn buffer(&self, display: &GlutinFacade) -> VertexBuffer<Vertex> {
-        return VertexBuffer::new(display, &self.shape()).unwrap();
+
+        let vertices: Vec<Vertex> =
+            (-90..91).map(|n| Vertex { position: [(n as f32) / 100.0, 0.0] }).collect();
+
+        return VertexBuffer::new(display, &vertices).unwrap();
     }
 
     pub fn back_buffer(&self, display: &GlutinFacade) -> VertexBuffer<Vertex> {
-        return VertexBuffer::new(display, &back()).unwrap();
+
+        let x = 1.0;
+        let y = 0.5;
+
+        let x1 = -x;
+        let x2 = x;
+        let y1 = -y;
+        let y2 = y;
+
+        let back = vec![
+	        Vertex { position: [x1, y1] },
+	        Vertex { position: [x2, y1] },
+	        Vertex { position: [x2, y2] },
+	        Vertex { position: [x1, y1] },
+	        Vertex { position: [x1, y2] },
+	        Vertex { position: [x2, y2] },
+	    ];
+
+        return VertexBuffer::new(display, &back).unwrap();
     }
 
     pub fn next(&self) -> AThing {
